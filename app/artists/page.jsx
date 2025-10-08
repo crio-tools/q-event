72a6aa5ec88b7a8881d0dda1b0d823c5fa9e181f
crio-tools <128483041+crio-tools@users.noreply.github.com>
2025-10-08 22:20:18 +0530

    Update page.jsx

--- app/artists/page.jsx
+++ app/artists/page.jsx
@@ -1,21 +1,31 @@
-import React from "react";
+'use client';
+
+import React, { useEffect, useState } from "react";
 import ArtistCard from "@/components/ArtistCard";
 
-async function ArtistsPage() {
-  
-  const data = [];
-  try {
-    const res = await fetch('https://qevent-backend.labs.crio.do/api/artists');
-    if (res.ok) data = await res.json();
-  } catch (e) {
-    console.error("Backend not reachable, skipping...");
-  }
-  const artists = await data.json();
-  
+function ArtistsPage() {
+  const [artists, setArtists] = useState([]);
+
+  useEffect(() => {
+    async function fetchArtists() {
+      try {
+        const res = await fetch('https://qevent-backend.labs.crio.do/api/artists');
+        if (res.ok) {
+          const data = await res.json();
+          setArtists(data);
+        }
+      } catch (e) {
+        console.error("Backend not reachable, skipping...");
+      }
+    }
+
+    fetchArtists();
+  }, []);
+
   return (
     <div className="h-full w-full flex-wrap flex items-center justify-around mt-8 mb-32">
       {artists.map((artistData) => (
-        <ArtistCard artistData={artistData} />
+        <ArtistCard key={artistData.id} artistData={artistData} />
       ))}
     </div>
   );
