ab278a461aa4a7e4a8492d0be399f7d91d9ae10f
crio-tools <128483041+crio-tools@users.noreply.github.com>
2025-10-08 22:18:30 +0530

    Update page.jsx

--- app/tags/page.jsx
+++ app/tags/page.jsx
@@ -1,29 +1,43 @@
-'use client'
+'use client';
+
 import Tag from "@/components/Tag";
 import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
 
-const TagsPage = async () => {
-
+const TagsPage = () => {
   const router = useRouter();
-  let tags = [];
-  try {
-    const res = await fetch('https://qevent-backend.labs.crio.do/api/tags');
-    if (res.ok) tags = await res.json();
-  } catch (e) {
-    console.error("Backend not reachable, skipping...");
-  }
-  
+  const [tags, setTags] = useState([]);
+
+  useEffect(() => {
+    async function fetchTags() {
+      try {
+        const res = await fetch('https://qevent-backend.labs.crio.do/api/tags');
+        if (res.ok) {
+          const data = await res.json();
+          setTags(data);
+        }
+      } catch (e) {
+        console.error("Backend not reachable, skipping...");
+      }
+    }
+
+    fetchTags();
+  }, []);
+
   return (
     <div className="h-[80vh] flex justify-center items-center">
       <div className="basis-3/4 p-4 flex justify-center flex-wrap gap-4">
         {tags.map((tag) => (
-          <div onClick={() => router.push(`/events?tag=${tag.name}`)}>
-          <Tag  text={tag.name} id={tag.id} />
+          <div key={tag.id} onClick={() => router.push(`/events?tag=${tag.name}`)}>
+            <Tag text={tag.name} id={tag.id} />
           </div>
         ))}
       </div>
     </div>
   );
 };
+
+export default TagsPage;
+
 
 export default TagsPage;
