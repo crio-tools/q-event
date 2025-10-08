34bb064ffc160048128431105029ed22d637ae72
crio-tools <128483041+crio-tools@users.noreply.github.com>
2025-10-08 22:10:51 +0530

    Update page.jsx

--- app/events/page.jsx
+++ app/events/page.jsx
@@ -1,5 +1,5 @@
-export const dynamic = "force-dynamic";
 'use client'
+export const dynamic = "force-dynamic";
 
 import React, { useState, useEffect } from "react";
 import EventCard from "@/components/EventCard";
